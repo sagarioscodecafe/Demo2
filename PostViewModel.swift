e3b30454089b96e7e2921525e61597ce7a6c2e94
sagarioscodecafe <131592368+sagarioscodecafe@users.noreply.github.com>
2024-04-26 20:08:05 +0530

    Update PostViewModel.swift

--- PostViewModel.swift
+++ PostViewModel.swift
@@ -5,13 +5,34 @@ class PostViewModel : ObservableObject {
 
   var currenPage = 1
   var itemsPerPage = 10
+  
   init() {
     fetchPosts()
   } 
   func fetchPosts() {
     guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts")
-    else {return}
-
-    
+    else {return} 
+    URLSession.shared.dataTask(with: url) {data, response, error in 
+                                          guard let data = data else {
+                                            print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
+                                            return }
+                                           do {
+                                             let decodeData = try JSONDecoder().decode([Post].self, from: data)
+                                             DispatchQueue.main.async{
+                                               self.posts.append(contentsOf: decodeData)
+                                               self.currentPage += 1
+                                               }
+                                           }
+                                           catch _ {
+                                             print("Error")
+                                           }
+                                          }.resume()
   }
+}
+
+
+extension PostViewModel {
+
+  func fetchpostsIfNeeded(item: Post?)
+  
 }
