1217c5a89e8874bc8ce6c6f3de3b37174cc73a3c
sagarioscodecafe <131592368+sagarioscodecafe@users.noreply.github.com>
2024-04-27 08:12:06 +0530

    Update PostViewModel.swift

--- PostViewModel.swift
+++ PostViewModel.swift
@@ -12,27 +12,43 @@ class PostViewModel : ObservableObject {
   func fetchPosts() {
     guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts")
     else {return} 
-    URLSession.shared.dataTask(with: url) {data, response, error in 
-                                          guard let data = data else {
-                                            print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
-                                            return }
-                                           do {
-                                             let decodeData = try JSONDecoder().decode([Post].self, from: data)
-                                             DispatchQueue.main.async{
-                                               self.posts.append(contentsOf: decodeData)
-                                               self.currentPage += 1
-                                               }
-                                           }
-                                           catch _ {
-                                             print("Error")
-                                           }
-                                          }.resume()
+    URLSession.shared.dataTask(with: url) { data, response, error in 
+            guard let data = data else {
+             print("No data in response: \(error?.localizedDescription ?? "Unknown error")")
+               return 
+            }
+              do {
+                    let decodedData = try JSONDecoder().decode([Post].self, from: data)
+                     DispatchQueue.main.async {
+                     self.posts.append(contentsOf: decodedData)
+                     self.currentPage += 1
+                  }
+            }                              
+            catch _ {
+              print("Error")
+          }
+        }.resume()
   }
 }
 
-
 extension PostViewModel {
 
-  func fetchpostsIfNeeded(item: Post?)
-  
+  func fetchPostsIfNeeded(item: Post?) {
+    guard let item = item else {
+      fetchPosts()
+      return
+    }
+    
+    if shouldFetchNextPage(currenItem:item) {
+      fetchPosts()
+    }
+  }
+    private func shouldFetchNextPage(currentItem: Post) -> Bool
+    {
+      guard let lastIndex = posts.lastIndex(where: { $0.id == currenItem.id }) else {
+      return false
+    }
+      let thresholdIndex = posts.index(posts.endIndex, offSetBy: -5)
+    return lastIndex == thresholdIndex
+  }
 }
